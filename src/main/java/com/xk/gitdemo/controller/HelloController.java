2622e11ec4fe31302fbbc5c3ff1c321df77c1711
chenxiaokang <948982432@qq.com>
2020-10-19 11:22:09 +0800

    第一次提交项目gitdemo

---
+++ src/main/java/com/xk/gitdemo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.xk.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+@RequestMapping("/info")
+    public String info(){
+        return "git   demo";
+    }
+}
